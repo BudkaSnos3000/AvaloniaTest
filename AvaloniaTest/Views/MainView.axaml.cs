9f96231e6a579aa4abd54f75598232728ac87759
BudkaSnos3000 <icontab80@gmail.com>
2023-10-27 11:45:57 +0300

    Добавьте файлы проекта.

---
+++ AvaloniaTest/Views/MainView.axaml.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia.Controls;
+using Avalonia.Interactivity;
+
+namespace AvaloniaTest.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+
+
+    public void Button1_OnClick(object? sender, RoutedEventArgs args)
+    {
+        
+    }
+}
